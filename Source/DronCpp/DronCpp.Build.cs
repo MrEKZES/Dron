fac002b4e4020ad6e9163c7cac0b9783a7f0732f
MrEKZES <powetkinw@gmail.com>
2026-05-09 19:42:22 +0300

    Place actors on level and some changes

--- Source/DronCpp/DronCpp.Build.cs
+++ Source/DronCpp/DronCpp.Build.cs
@@ -5,7 +5,12 @@ public class DronCpp : ModuleRules
 	public DronCpp(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-    
+		
+		if (Target.Configuration != UnrealTargetConfiguration.Shipping)
+		{
+			OptimizeCode = CodeOptimization.InShippingBuildsOnly;
+		}
+
 		PublicDependencyModuleNames.AddRange(new string[] { 
 			"Core", 
 			"CoreUObject", 
@@ -20,5 +25,11 @@ public class DronCpp : ModuleRules
 		});
         
 		PrivateDependencyModuleNames.AddRange(new string[] { });
+		
+		if (Target.bBuildEditor)
+		{
+			MinFilesUsingPrecompiledHeaderOverride = 1;
+			bUseUnity = false;
+		}
 	}
 }
